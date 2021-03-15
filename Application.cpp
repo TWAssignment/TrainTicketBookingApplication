e954a9dfa5a8687b070204232ddb4581096af607
Pranav Shah <40690543+shahpranav1094@users.noreply.github.com>
2021-03-15 15:16:14 +0530

    Initial Commit

--- Application.cpp
+++ Application.cpp
@@ -22,3 +22,57 @@ int main()
 
 	return 0;
 }
+
+/*
+OUTPUT
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Guindy
+        Chennai Fort
+        Number of stops : 8 stops
+        Distance travelled : 405 kms
+        Journey fare : 15
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Kodambakkam
+        Guindy
+        Number of stops : 3 stops
+        Distance travelled : 175 kms
+        Journey fare : 10
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Chennai Beach
+        Tambaram
+        Chennai Beach
+        Number of stops : 34 stops
+        Distance travelled : 1870 kms
+        Journey fare : 35
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Tambaram
+        Chennai Fort
+        Number of stops : 16 stops
+        Distance travelled : 835 kms
+        Journey fare : 25
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Chennai Fort
+        Chennai Park
+        Number of stops : 1 stops
+        Distance travelled : 50 kms
+        Journey fare : 10
+
+Ticket Booked
+        15-3-2021  15:14:44
+        Chennai Park
+        Chennai Fort
+        Number of stops : 1 stops
+        Distance travelled : 50 kms
+        Journey fare : 10
+
+*/
